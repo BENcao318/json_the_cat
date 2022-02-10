7f159f2a2fa80834079764746579d9e990c472b0
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-02-09 20:40:19 -0500

    add test section using M&C and modulate the code.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,25 @@
-const request = require('request');
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
+const request = require("request");
 
-request(url, (error, response, body) => {
-  console.log('statusCode:', response && response.statusCode);
-  if(error) {
-    console.log('error: ', error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(typeof data);
-    if(!data.length) {
-      console.log('Breed not found');
-    } else{
-      console.log(data);
+const url = `https://api.thecatapi.com/v1/breeds/search?q=`;
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(url + breedName, (error, response, body) => {
+    // console.log('statusCode:', response && response.statusCode);
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (!data.length) {
+        callback("Breed not found", null);
+      } else {
+        callback(null, data[0].description);
+      }
+      
     }
-  }
-})
+  });
+};
+
+module.exports = {
+  fetchBreedDescription,
+};
+
